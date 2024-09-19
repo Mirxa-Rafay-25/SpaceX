b49e190a98d683e68f07eb54b0bf3a7b8649af9b
Mirxa-Rafay-25 <MirxaRafay25@gmail.com>
2024-09-19 16:06:43 +0500

    "add space"

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Skills from "@/components/main/Skills";
 import Image from "next/image";
 
 export default function Home() {
+  
   return (
 <main className="h-full w-full ">
    <div className="flex flex-col h-[800px] gap-20">
